d52240fa1a9bada704b9a67a680fb5fc5f94b219
midethegreat <shoremiayomide13@gmail.com>
2025-12-26 12:42:21 +0100

    commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,16 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// }
 
-export default config
+// export default config
+
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
